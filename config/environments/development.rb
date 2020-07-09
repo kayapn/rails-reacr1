4542c689c27a611cf420587b6ea1a03557d21084
kayapn <kayapn@yahoo.co.jp>
2020-07-09 06:20:47 +0000

    dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "3000-f760be68-d594-47b9-bd33-f32b62b5019c.ws-us02.gitpod.io"
 end
